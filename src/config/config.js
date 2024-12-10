09ffa86b073980845e8603a9653db147a6ecb97c
Sneh674 <naiksneh6@gmail.com>
2024-12-10 16:30:40 +0530

    signed jwt token

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,7 @@ const _config={
     port: process.env.PORT,
     databaseURL: process.env.MONGO_CONNECTION_STRING,
     env: process.env.NODE_ENV,//to determine dev or prod environment
+    jwtSecret: process.env.JWT_SECRET,
 };
 
 export const config = Object.freeze(_config);
--- src/user/userController.js
+++ src/user/userController.js
@@ -1,6 +1,10 @@
 import createHttpError from "http-errors";
 import userModel from "./userModel.js";
 import bcrypt from 'bcrypt'
+// import { sign } from 'jsonwebtoken'
+import pkg from 'jsonwebtoken';
+const { sign } = pkg;
+import { config } from "../config/config.js";
 
 const createUser=async(req,res,next)=>{ 
     const {name,email,password}=req.body;
@@ -13,11 +17,21 @@ const createUser=async(req,res,next)=>{
         const error = createHttpError (400,"User already exists");
         return next(error);
     }
+
     // 10 -> SALT ROUNDS
     const hashedPassword = await bcrypt.hash(password, 10)
+
+    const newUser =await userModel.create({
+        name,
+        email,
+        password: hashedPassword
+    })
     
+    const token=sign({sub: newUser._id}, config.jwtSecret, {expiresIn: '7d'})
+
     res.json({
-        message: "User Created"
+        id: newUser._id,
+        accesstoken: token
     })
 }
 
