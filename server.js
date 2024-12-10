aef6e8d59d20c725fea7b1ce398eff2c4f000565
Sneh674 <naiksneh6@gmail.com>
2024-12-10 14:26:23 +0530

    db setup

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import app from "./src/app.js"
 import { config } from "./src/config/config.js"
+import connectDb from "./src/config/db.js"
 
-const startServer=()=>{
+const startServer=async()=>{
+    await connectDb();
     const port = config.port || 3000
 
     app.listen(port, ()=>{
---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import { config } from "./config.js"
+
+const connectDb=async()=>{
+    try{
+        mongoose.connection.on("connected",()=>{
+            console.log("connected to databse")
+        })
+        mongoose.connection.on('error',(err)=>{
+            console.log("Error in connecting database",err)
+        })
+        await mongoose.connect(config.databaseURL);
+    } catch(err){
+        console.log("Error connecting to mongo db",err)
+        process.exit(1);//stop the server because can't connect to db
+    }
+}
+
+export default connectDb
