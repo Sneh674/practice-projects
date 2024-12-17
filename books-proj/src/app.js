dca9b1ab8744c6050df5cfe6dcf02cfe5ae5c494
Sneh674 <naiksneh6@gmail.com>
2024-12-17 14:04:30 +0530

    add cors for deployed fe

--- books-proj/src/app.js
+++ books-proj/src/app.js
@@ -1,22 +1,38 @@
-import express from 'express'
-import globalErrorHandler from './middlewares/globalErrorHandler.js'
-import userRouter from './user/userRouter.js'
-import bookRouter from './book/bookRouter.js'
-import cors from 'cors';
+import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+import userRouter from "./user/userRouter.js";
+import bookRouter from "./book/bookRouter.js";
+import cors from "cors";
 
-const app=express()
+const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
+// app.use(cors({
+//     origin: "http://localhost:5173"
+// }))
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "https://localhost:5173",
+        "https://book-frontend-nlvi.onrender.com",
+      ];
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.json({message: "welcome to the apis"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to the apis" });
+});
 
-app.use("/api/users",userRouter)
-app.use("/api/books",bookRouter)
-app.use(globalErrorHandler)
+app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
+app.use(globalErrorHandler);
 
 export default app;
